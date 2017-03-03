f95c031bd47ccd0a27e1b91e0c63c419c0085b01
Riccardo Ricci <zio.rick@gmail.com>
2017-03-03 16:18:56 +0100

    Modifica utente

--- src/main/java/com/nexse/mail/App.java
+++ src/main/java/com/nexse/mail/App.java
@@ -5,14 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
-    public static final String name = "Ivano";
-    public static final String surname = "Rosi";
-    public static final String uid = "ivano.rossi";
+    public static final String name = "Christian";
+    public static final String surname = "Fiorillo";
+    public static final String uid = "christian.fiorillo";
     public static final String domain = "nexse.com";
     //public static final String domain = "nexsesolutions.com";
     //public static final String domain = "w-lab.it";
-    public static final String password = "U18Pn9al8LgW7iT2";
-    public static final String personalEmail = "ivano.rossi@libero.it";
+    public static final String password = "cambialasubito";
+    public static final String personalEmail = "fiorillo.christian@gmail.com";
 
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
